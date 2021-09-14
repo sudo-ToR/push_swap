228b50b6321da273061cd0b9bae59b8c5e0d9ab0
lina noirot <lina.noirot@gmail.com>
2021-09-14 16:49:56 +0200

    begin algo for 5 and less but still some issues

--- srcs/algo/sorting.c
+++ srcs/algo/sorting.c
@@ -6,7 +6,7 @@
 /*   By: tor <tor@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 13:57:19 by tor               #+#    #+#             */
-/*   Updated: 2021/09/13 16:26:01 by tor              ###   ########.fr       */
+/*   Updated: 2021/09/14 16:47:51 by tor              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,36 @@ void	sorting_little(t_push_swap *main)
 	{
 		if (!is_bigger(main->a->table[0], main->a->table[1]))
 			swap_a(main);
-		else
-			break;
+		else if (last_in_table(main->a) > main->a->table[0])
+		{
+			r_rotate_a(main);
+			swap_a(main);
+		}
+		while (main->a->table[0] < main->a->table[1] && !is_sorted_in_circle_decres(main->a))
+			rotate_a(main);
 	}
 }	
+
+void	put_min_first(char stack_name, t_int_table *stack)
+{
+	int		i;
+	int		min;
+
+	i = 0;
+	min = ft_min(stack);
+	while (stack->table[i] != min)
+		i++;
+	if (i < stack->len / 2)
+	{
+		while (stack->table[0] != min)
+			rotate(stack_name, stack);
+	}
+	else
+	{
+		while (stack->table[0] != min)
+			r_rotate(stack_name, stack);
+	}
+}
 
 void	sorting_a(t_push_swap *main)
 {
@@ -31,4 +57,5 @@ void	sorting_a(t_push_swap *main)
 	
 	if (main->a->len <= 5)
 		sorting_little(main);
+	put_min_first('a', main->a);
 }
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tor <tor@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/20 17:26:14 by lnoirot           #+#    #+#             */
-/*   Updated: 2021/09/13 16:31:25 by tor              ###   ########.fr       */
+/*   Updated: 2021/09/14 16:45:49 by tor              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,47 +23,48 @@ void	free_struct_push_swap(t_push_swap *to_free)
 int	main(int ac, char **av)
 {
 	t_push_swap		main;
-	char			*line;
-	int				i;
+	// char			*line;
+	// int				i;
 
-	i = 0;
+	// i = 0;
 	if (ac < 2)
 		return (0);
 	ft_memset(&main, 0, sizeof(t_push_swap));
 	parsing(&av[1], &main);
+	// int min = ft_min(main.a);
+	// int max = ft_max(main.a);
+	// int med = ft_median(main.a);
+	sorting_a(&main);
 	print_stack(main.a, main.b);
-	int min = ft_min(main.a);
-	int max = ft_max(main.a);
-	int med = ft_median(main.a);
-	while (get_next_line(0, &line))
-	{
-		if (!ft_strcmp(line, "sa"))
-			swap_a(&main);
-		else if (!ft_strcmp(line, "sb"))
-			swap_b(&main);
-		else if (!ft_strcmp(line, "ss"))
-			d_swap(&main);
-		else if (!ft_strcmp(line, "pb"))
-			push_b(&main);
-		else if (!ft_strcmp(line, "pa"))
-			push_a(&main);
-		else if (!ft_strcmp(line, "ra"))
-			rotate_a(&main);
-		else if (!ft_strcmp(line, "rb"))
-			rotate_b(&main);
-		else if (!ft_strcmp(line, "rr"))
-			d_rotate(&main);
-		else if (!ft_strcmp(line, "rra"))
-			r_rotate_a(&main);
-		else if (!ft_strcmp(line, "rrb"))
-			r_rotate_b(&main);
-		else if (!ft_strcmp(line, "rrr"))
-			r_drotate(&main);	
-		i++;
-	printf("min : %d\t\tmax = %d\t\tmed = %d\n", min, max, med);
-		print_stack(main.a, main.b);
-		printf("mvt = %d\t\tsorted = %d\n", i, is_sorted_in_circle_decres(main.a));
-	}
+	// while (get_next_line(0, &line))
+	// {
+	// 	if (!ft_strcmp(line, "sa"))
+	// 		swap_a(&main);
+	// 	else if (!ft_strcmp(line, "sb"))
+	// 		swap_b(&main);
+	// 	else if (!ft_strcmp(line, "ss"))
+	// 		d_swap(&main);
+	// 	else if (!ft_strcmp(line, "pb"))
+	// 		push_b(&main);
+	// 	else if (!ft_strcmp(line, "pa"))
+	// 		push_a(&main);
+	// 	else if (!ft_strcmp(line, "ra"))
+	// 		rotate_a(&main);
+	// 	else if (!ft_strcmp(line, "rb"))
+	// 		rotate_b(&main);
+	// 	else if (!ft_strcmp(line, "rr"))
+	// 		d_rotate(&main);
+	// 	else if (!ft_strcmp(line, "rra"))
+	// 		r_rotate_a(&main);
+	// 	else if (!ft_strcmp(line, "rrb"))
+	// 		r_rotate_b(&main);
+	// 	else if (!ft_strcmp(line, "rrr"))
+	// 		r_drotate(&main);	
+	// 	i++;
+	// printf("min : %d\t\tmax = %d\t\tmed = %d\n", min, max, med);
+	// 	print_stack(main.a, main.b);
+	// 	printf("mvt = %d\t\tsorted = %d\n", i, is_sorted_in_circle_decres(main.a));
+	// }
 	free_struct_push_swap(&main);
 	return (0);
 }
